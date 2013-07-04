e7e930dc58aaf74c02f42f3ebed0a2d238dc2f3b
dtbullock <david.bullock@machaira.com.au>
2013-07-05 02:10:06 +1000

    Initial import.

--- test.php
+++ test.php
@@ -2,4 +2,7 @@
 echo (int)ini_get('zlib.output_compression');
 @ini_set('zlib.output_compression_level', 1);
 echo (int)ini_get('zlib.output_compression');
+echo ini_get('output_buffering');
 echo 'done';
+
+phpinfo();
